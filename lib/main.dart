c4a66b3b829f1fe223a451e8fec3ab93cb2688ff
Anatoliy Shvarts <shvarts.tolik@mail.ru>
2023-03-05 18:48:39 +0300

    Add info page

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class MyAppState extends ChangeNotifier {
   }
 
   void initJokeList() async{
-    if (cards.isEmpty){
+    if (cards.length < _arrLength){
       for (int i = 0; i < _arrLength; i++) {
         print(i);
         var result = await http.get(
@@ -92,7 +92,7 @@ class MyAppState extends ChangeNotifier {
         cards.add(_getJokeCard(currentJokeModel));
       }
     }
-    notifyListeners();
+    // notifyListeners();
   }
 
   void updateJokeList(int index) async {
@@ -126,21 +126,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var pageIndex = 1;
+  var pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    var colorScheme = Theme.of(context).colorScheme;
     var appState = context.watch<MyAppState>();
+    appState.initJokeList();
 
     Widget page;
     switch (pageIndex) {
       case 0:
-        page = SettingsPage();
+        page = InfoPage();
         break;
       case 1:
         page = JokePage();
-        appState.initJokeList();
         break;
       case 2:
         page = FavoritesPage();
@@ -159,8 +158,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: BottomNavigationBar(
                     items: [
                       BottomNavigationBarItem(
-                        icon: Icon(Icons.settings),
-                        label: 'Settings',
+                        icon: Icon(Icons.info),
+                        label: 'Info',
                       ),
                       BottomNavigationBarItem(
                         icon: Icon(Icons.home),
@@ -187,17 +186,99 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class SettingsPage extends StatelessWidget{
+class InfoPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    var theme = Theme.of(context);
-    var appState = context.watch<MyAppState>();
+    var colorScheme = Theme.of(context).colorScheme;
 
-    return Center(
-      child: Text('No setting yet.'),
+    return ListView(
+      children: <Widget>[
+        Container(
+          height: 400,
+          decoration: BoxDecoration(
+            gradient: gradientRed
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Anatoliy Shvarts',
+                style: TextStyle(
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Chuck Norris joke app',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(
+                'Telegram',
+                style: TextStyle(
+                  color: colorScheme.primary,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                '@quiner123',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text(
+                'Email',
+                style: TextStyle(
+                  color: colorScheme.primary,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                'a.shvarts@innopolis.university',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text(
+                'GitHub',
+                style: TextStyle(
+                  color: colorScheme.primary,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text(
+                'https://github.com/quiner1793/tinder_with_chuck',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
+
 
 
 class JokePage extends StatelessWidget {
