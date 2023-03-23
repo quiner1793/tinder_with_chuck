c253e4dd89c563287ee440bd1673ec4bc43f9647
Anatoliy Shvarts <shvarts.tolik@mail.ru>
2023-03-23 04:10:14 +0300

    Add logout and search buttons

--- lib/pages/joke_page.dart
+++ lib/pages/joke_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
@@ -8,6 +9,11 @@ import '../provider/joke_provider.dart';
 import '../widgets/joke_card.dart';
 
 class JokePage extends ConsumerWidget {
+
+  Future<void> _signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
   void _swipeJoke(
       int index, CardSwiperDirection direction, WidgetRef ref) async {
     var currentJoke = ref.watch(jokesProvider).jokes[index];
@@ -15,6 +21,10 @@ class JokePage extends ConsumerWidget {
       ref.read(favoritesProvider.notifier).addFavorite(currentJoke);
     }
     ref.read(jokesProvider.notifier).updateJokeList(index);
+  }
+
+  void _showSearch(){
+    print("Search");
   }
 
   @override
@@ -28,46 +38,56 @@ class JokePage extends ConsumerWidget {
         child: SizedBox(
             width: 30, height: 30, child: CircularProgressIndicator()))
         : CupertinoPageScaffold(
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 50,
+        navigationBar:  CupertinoNavigationBar(
+          leading: IconButton(
+            onPressed: _signOut,
+            icon: Icon(Icons.logout_rounded),
           ),
-          Expanded(
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * 0.75,
-                child: CardSwiper(
-                  controller: controller,
-                  cards: cards,
-                  onSwipe: (int index, CardSwiperDirection direction) {
-                    _swipeJoke(index, direction, ref);
-                  },
-                  isVerticalSwipingEnabled: false,
-                  isLoop: true,
-                  padding: const EdgeInsets.only(
-                    left: 25,
-                    right: 25,
-                    top: 50,
-                    bottom: 40,
+          trailing: IconButton(
+            onPressed: _showSearch,
+            icon: Icon(Icons.search_rounded),
+          ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Expanded(
+                child: SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  child: CardSwiper(
+                    controller: controller,
+                    cards: cards,
+                    onSwipe: (int index, CardSwiperDirection direction) {
+                      _swipeJoke(index, direction, ref);
+                    },
+                    isVerticalSwipingEnabled: false,
+                    isLoop: true,
+                    padding: const EdgeInsets.only(
+                      left: 25,
+                      right: 25,
+                      top: 50,
+                      bottom: 40,
+                    ),
                   ),
-                ),
-              )),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              FloatingActionButton(
-                  onPressed: controller.swipeLeft,
-                  child: const Icon(Icons.close)),
-              FloatingActionButton(
-                  onPressed: controller.swipeRight,
-                  child: const Icon(Icons.favorite)),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-        ],
-      ),
+                )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                    onPressed: controller.swipeLeft,
+                    child: const Icon(Icons.close)),
+                FloatingActionButton(
+                    onPressed: controller.swipeRight,
+                    child: const Icon(Icons.favorite)),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
     );
   }
 }
